91bc23cc6a119ab20630aa365f1abc71627e354c
dnns <dennis.ah@gmail.com>
2023-05-11 16:36:35 -0500

    Inicial base Cliente

---
+++ src/main/java/proyecto/bootcamp/banca/cliente/ClienteApplication.java
@@ -0,0 +1,13 @@
+package proyecto.bootcamp.banca.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/bootcamp/banca/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.bootcamp.banca.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
